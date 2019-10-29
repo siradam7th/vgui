af7b4aa60f8d8180385144a0e8b6e2a44dfb4691
siradam7th <siradam7th@users.noreply.github.com>
2019-10-29 04:54:39 +0100

    add label background drawing, fix label not being updated after a change

--- vgui/elements/label.cpp
+++ vgui/elements/label.cpp
@@ -18,6 +18,9 @@ Label::Label(const std::string& text, const std::string& font_name, vec2f pos,
         // draw borders
         graphics_context->fillRectRounded(m_style.borders_radius, m_borders_rect, m_style.borders_color);
 
+        // draw background
+        graphics_context->fillRectRounded(m_style.borders_radius, m_rect, m_style.bg_color);
+
         // draw text
         graphics_context->fillText(m_text, m_font_size, m_font_name, { m_rect.x, m_rect.y }, m_style.color, m_font_style);
     };
@@ -81,30 +84,41 @@ vec4f Label::calcTextBounds(const vec2f& pos, const std::string& text, const std
 void Label::setText(const std::string& text)
 {
     m_text = text;
-    // recalculate text bounds
-    m_rect = calcTextBounds({ m_rect.x, m_rect.y }, text, m_font_name, m_font_size);
+    // issue an update
+    if (m_draw_events.on_update)
+    {
+        m_draw_events.on_update();
+    }
 }
 
 void Label::setFontName(const std::string& font_name)
 {
     m_font_name = font_name;
-    // recalculate text bounds
-    m_rect = calcTextBounds({ m_rect.x, m_rect.y }, m_text, m_font_name, m_font_size);
+    // issue an update
+    if (m_draw_events.on_update)
+    {
+        m_draw_events.on_update();
+    }
 }
 
 void Label::setFontSize(float size)
 {
     m_font_size = size;
-    // recalculate text bounds
-    m_rect = calcTextBounds({ m_rect.x, m_rect.y }, m_text, m_font_name, m_font_size);
+    // issue an update
+    if (m_draw_events.on_update)
+    {
+        m_draw_events.on_update();
+    }
 }
 
 void Label::setFontStyle(FontStyle font_style)
 {
     m_font_style = font_style;
-    // TODO: uncomment calcTextBounds() line once FontStyle is supported here in Label
-    // recalculate text bounds
-    //m_rect = calcTextBounds({ m_rect.x, m_rect.y }, m_text, m_font_name, m_font_size);
+    // issue an update
+    if (m_draw_events.on_update)
+    {
+        m_draw_events.on_update();
+    }
 }
 
 void Label::setStyle(Style style)
